f85387374dc2cfd21f0169015e3fb230d24031cc
Nico De Witte <nico.dewitte@outlook.com>
2020-11-24 12:07:03 +0100

    Add code

---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Strings and Objects
+
+Demo code of Strings and objects
+
+## How to get this locally
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/StringObjectDemo.git
+```
---
+++ StringObjectDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StringObjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string hallo = new string("Hello");
+
+            if (hello == hallo)     // INHOUDELIJKE VERGELIJKING
+            {
+                Console.WriteLine("Both are equal");
+            }
+
+            int[] number = { 1, 2, 3 };
+            int[] values = { 1, 2, 3 };
+
+            if (number == values)       // REFERENTIES WORDEN VERGELEKEN
+            {
+                Console.WriteLine("Numbers == values");
+            }
+
+            Random generator1 = new Random();
+            Random generator2 = generator1;
+            Random generator3 = new Random();
+
+            if (generator1 == generator2)
+            {
+                Console.WriteLine("generator1 == generator2");
+            }
+
+            if (generator1 == generator3)
+            {
+                Console.WriteLine("generator1 == generator3");
+            }
+
+            int a = 5;
+            int b = a;
+
+            if (b == a)
+            {
+                Console.WriteLine("1. b == a");
+            }
+
+            b = 6;
+
+            if (b == a)
+            {
+                Console.WriteLine("2. b == a");
+            }
+        }
+    }
+}
